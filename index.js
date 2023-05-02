9240580e546434b55febb4781e1ce15723139cf4
ramicurros <ramicurros@gmail.com>
2023-05-02 13:54:04 -0300

    changes commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
